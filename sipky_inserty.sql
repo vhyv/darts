2cc5eaba97f852dd58051e24e27a1ebe61473b37
Vojtěch Hyvnar <vhyvnar@gmail.com>
2018-04-01 15:42:23 +0200

    Add files via upload

---
+++ sipky_inserty.sql
@@ -0,0 +1,7 @@
+USE sipky;
+GO
+
+INSERT INTO zapas(datum, player1, player2, result, typ_hry, typ_otvor, pocet_setu, pocet_legu)
+
+INSERT INTO detaily(id_zapas, player, vyhrane_sety, vyhrane_legy, maximum, prumer, prumer6_9, nejrychlejsi, finish, [100plus], [140plus]
+	,[180], topavg, botavg,  [20minus], [20plus], [40plus], [60plus], [80plus])
--- view_zapasy_uspesnost.sql
+++ view_zapasy_uspesnost.sql
@@ -1,9 +1,30 @@
 USE sipky;
 GO
 
+/*
+			Datum:			2018/04/01
+								
+			Zobrazuje:
+							Pocet legu planovanych
+							Pocet legu odehranych
+							Hrac 1
+							Legy hrace 1
+							Hrac 2
+							Legy hrace 2
+							Vitezny hrac
+							Pocet her viteze (kumul)
+							Pocet vyher viteze (kumul)
+							Uspesnost viteze (kumul)
+
+			Ñezobrazuje:	Legy CPU oponenta
+							Odehrane legy proti CPU oponentovi
+							Jsem lenoch 
+*/
+
 
 CREATE VIEW zapasy_uspesnost
 AS
+
 WITH t AS (
 	SELECT player1 AS player, datum, id_zapas
 	FROM zapas
@@ -20,8 +41,17 @@ FROM t
 
 )
 , vyhranezapasy AS (
-SELECT z.id_zapas, z.datum, p.nickname AS hrac1, pp.nickname AS hrac2, ppp.nickname AS vitez, ph.pocetherviteze
-, COUNT(ppp.nickname) OVER(PARTITION BY result ORDER BY z.datum ASC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS pocetvyherviteze
+SELECT z.id_zapas
+	, z.datum
+	, z.pocet_legu
+	, SUM(d.vyhrane_legy + dd.vyhrane_legy) OVER(PARTITION BY z.id_zapas) AS odehrane_legy
+	, p.nickname AS hrac1
+	, d.vyhrane_legy AS legy1
+	, pp.nickname AS hrac2
+	, dd.vyhrane_legy AS legy2
+	, ppp.nickname AS vitez
+	, ph.pocetherviteze
+	, COUNT(ppp.nickname) OVER(PARTITION BY result ORDER BY z.datum ASC, z.id_zapas ASC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS pocetvyherviteze
 
 FROM zapas AS z
 	INNER JOIN players AS p
@@ -34,6 +64,12 @@ FROM zapas AS z
 				ON z.result = ph.player
 				AND z.datum = ph.datum
 				AND z.id_zapas = ph.id_zapas
+					LEFT OUTER JOIN detaily AS d
+					ON z.id_zapas = d.id_zapas
+					AND z.player1 = d.player
+						LEFT OUTER JOIN detaily AS dd
+						ON z.id_zapas = dd.id_zapas
+						AND z.player2 = dd.player
 )
 SELECT *, CONCAT(CAST(pocetvyherviteze*100.0/pocetherviteze AS numeric(5,2)), ' %') AS uspesnost
 FROM vyhranezapasy
